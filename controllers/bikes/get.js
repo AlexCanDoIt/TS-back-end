73fff9ed13d21a1d6f7513db54756c019196b2c1
AlexCanDoIt <alexcandoitt@gmail.com>
2021-09-05 13:08:47 +0300

    Added total in ctrl/bike/get

--- controllers/bikes/get.js
+++ controllers/bikes/get.js
@@ -4,13 +4,17 @@ const get = async (req, res, next) => {
   const { query } = req
 
   try {
+    let total = 0
     const bikes = await bikeService.get(query)
     const types = await typeService.get()
     const result = bikes.map(bike => {
+      if (bike.rented) { total = total + bike.price }
       const typeObj = types.find(type => String(type._id) === String(bike.type))
+
       return {
         ...bike._doc,
-        type: typeObj
+        type: typeObj,
+        total
       }
     })
 
